97cb6159d8d69eb48607b64e9b1d7f8e4d22ddd0
kinath <kinath.ru@gmail.com>
2024-12-22 17:19:38 -0600

    feat:add app db context

--- EmployeeManagement/EmployeeManagement/Program.cs
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeeManagement;
 
 public class Program
@@ -5,6 +8,12 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("EmployeeDb");
+        });
+        
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
