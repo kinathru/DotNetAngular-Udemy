23a544551947a5980037cf02827ba011e4f65e15
kinath <kinath.ru@gmail.com>
2024-12-22 16:57:11 -0600

    feat:enable cors

--- BookApi/BookApi/Program.cs
+++ BookApi/BookApi/Program.cs
@@ -8,11 +8,21 @@ public class Program
         
         // add services
         builder.Services.AddControllers();
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("MyCors", builder =>
+            {
+                builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+        });
         
         var app = builder.Build();
 
         // add mapping
         app.MapControllers();
+        app.UseCors("MyCors");
         app.MapGet("/", () => Results.Redirect("/api/books"));
 
         app.Run();
