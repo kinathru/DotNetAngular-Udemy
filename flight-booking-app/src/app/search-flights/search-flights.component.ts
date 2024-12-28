d8c4a8d69ca43fc38cb4980d02a3f7a19f636ee2
kinath <kinath.ru@gmail.com>
2024-12-28 09:37:14 -0600

    feat:display a list of flights

--- flight-booking-app/src/app/search-flights/search-flights.component.ts
+++ flight-booking-app/src/app/search-flights/search-flights.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-flights',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-flights.component.html',
-  styleUrl: './search-flights.component.css'
+  styleUrl: './search-flights.component.css',
 })
-export class SearchFlightsComponent {
+export class SearchFlightsComponent implements OnInit {
+  searchResult: any = ['American Airlines', 'British Airways', 'Lufthansa'];
 
+  ngOnInit(): void {}
 }
