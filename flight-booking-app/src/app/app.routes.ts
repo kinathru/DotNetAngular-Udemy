a8c11349dfce263cf850bc3e130e65296aaed138
kinath <kinath.ru@gmail.com>
2024-12-28 09:27:29 -0600

    feat:set up navigation for flight search component

--- flight-booking-app/src/app/app.routes.ts
+++ flight-booking-app/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: SearchFlightsComponent }];
