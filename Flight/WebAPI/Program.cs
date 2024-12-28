630b1a89c6a2b3761756445bbe03c44e8e105820
kinath <kinath.ru@gmail.com>
2024-12-28 11:03:13 -0600

    feat:configure swagger and api controllers

--- Flight/WebAPI/Program.cs
+++ Flight/WebAPI/Program.cs
@@ -5,9 +5,39 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                corsPolicyBuilder =>
+                {
+                    corsPolicyBuilder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+        });
+
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/hello", () => "Hello World!");
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                options.RoutePrefix = string.Empty; // display swagger UI for root path
+            });
+        }
+
+        app.UseCors("CorsPolicy");
+        app.MapControllers();
 
         app.Run();
     }
