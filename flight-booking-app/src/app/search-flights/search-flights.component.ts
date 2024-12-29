0a7c7bfa9b7df63ca99fc8ca8ecd7a82c3ba7524
kinath <kinath.ru@gmail.com>
2024-12-29 08:06:00 -0600

    feat:use web api to retrieve flight search results

--- flight-booking-app/src/app/search-flights/search-flights.component.ts
+++ flight-booking-app/src/app/search-flights/search-flights.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlightService } from '../api/services';
+import { FlightRm } from '../api/models';
 
 @Component({
   selector: 'app-search-flights',
@@ -9,91 +11,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './search-flights.component.css',
 })
 export class SearchFlightsComponent implements OnInit {
-  searchResult: FlightRm[] = [
-    {
-      airline: 'Airways Alpha',
-      arrival: { place: 'New York', time: '2024-12-28T14:30:00' },
-      departure: { place: 'Los Angeles', time: '2024-12-28T10:00:00' },
-      price: '$450',
-      remainingNumberOfSeats: 20,
-    },
-    {
-      airline: 'Bravo Airlines',
-      arrival: { place: 'Chicago', time: '2024-12-28T18:00:00' },
-      departure: { place: 'Houston', time: '2024-12-28T15:00:00' },
-      price: '$320',
-      remainingNumberOfSeats: 15,
-    },
-    {
-      airline: 'SkyHigh',
-      arrival: { place: 'San Francisco', time: '2024-12-28T13:45:00' },
-      departure: { place: 'Seattle', time: '2024-12-28T11:00:00' },
-      price: '$280',
-      remainingNumberOfSeats: 25,
-    },
-    {
-      airline: 'Delta Express',
-      arrival: { place: 'Miami', time: '2024-12-28T19:30:00' },
-      departure: { place: 'Atlanta', time: '2024-12-28T17:00:00' },
-      price: '$400',
-      remainingNumberOfSeats: 10,
-    },
-    {
-      airline: 'Echo Flights',
-      arrival: { place: 'Boston', time: '2024-12-28T20:15:00' },
-      departure: { place: 'Denver', time: '2024-12-28T16:30:00' },
-      price: '$380',
-      remainingNumberOfSeats: 5,
-    },
-    {
-      airline: 'Foxtrot Wings',
-      arrival: { place: 'Las Vegas', time: '2024-12-28T12:30:00' },
-      departure: { place: 'Phoenix', time: '2024-12-28T10:00:00' },
-      price: '$150',
-      remainingNumberOfSeats: 30,
-    },
-    {
-      airline: 'Global Air',
-      arrival: { place: 'Orlando', time: '2024-12-28T21:00:00' },
-      departure: { place: 'Dallas', time: '2024-12-28T18:00:00' },
-      price: '$310',
-      remainingNumberOfSeats: 8,
-    },
-    {
-      airline: 'Horizon',
-      arrival: { place: 'Salt Lake City', time: '2024-12-28T15:45:00' },
-      departure: { place: 'Portland', time: '2024-12-28T13:00:00' },
-      price: '$220',
-      remainingNumberOfSeats: 12,
-    },
-    {
-      airline: 'Inland Airways',
-      arrival: { place: 'Washington D.C.', time: '2024-12-28T22:30:00' },
-      departure: { place: 'Charlotte', time: '2024-12-28T20:00:00' },
-      price: '$330',
-      remainingNumberOfSeats: 7,
-    },
-    {
-      airline: 'Jet Stream',
-      arrival: { place: 'Minneapolis', time: '2024-12-28T23:45:00' },
-      departure: { place: 'Detroit', time: '2024-12-28T21:15:00' },
-      price: '$270',
-      remainingNumberOfSeats: 18,
-    },
-  ];
+  searchResult: FlightRm[] = [];
+
+  constructor(private flightService: FlightService) {}
 
   ngOnInit(): void {}
-}
 
-export interface FlightRm {
-  airline: string;
-  arrival: TimePlaceRm;
-  departure: TimePlaceRm;
-  price: string;
-  remainingNumberOfSeats: number;
-}
-
-export interface TimePlaceRm {
-  place: string;
-  time: string;
+  search() {
+    this.flightService.flightGet().subscribe((response) => {
+      this.searchResult = response;
+    });
+  }
 }
