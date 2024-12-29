70e734227b9d4462fdb8bf95bd7fca51f27bdded
kinath <kinath.ru@gmail.com>
2024-12-29 09:10:01 -0600

    feat:add book flight component

---
+++ flight-booking-app/src/app/book-flight/book-flight.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-flight',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-flight.component.html',
+  styleUrl: './book-flight.component.css'
+})
+export class BookFlightComponent implements OnInit{
+
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
