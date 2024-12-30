bff71060cc09b88e87880d7ac72499a2fba472a8
kinath <kinath.ru@gmail.com>
2024-12-29 22:26:08 -0600

    feat:registering a user

---
+++ Flight/WebAPI/Controllers/Dtos/PassengerRm.cs
@@ -0,0 +1,3 @@
+﻿namespace WebAPI.Controllers.Dtos;
+
+public record PassengerRm(string Email, string FirstName, string LastName, bool Gender);
---
+++ Flight/WebAPI/Controllers/PassengerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Controllers.Dtos;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PassengerController : ControllerBase
+{
+    private static readonly List<NewPassengerDto> Passengers = new();
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public ActionResult<NewPassengerDto?> Register(NewPassengerDto dto)
+    {
+        Passengers.Add(dto);
+        System.Diagnostics.Debug.WriteLine(Passengers.Count);
+        return CreatedAtAction(nameof(Find), new { email = dto.Email }, dto);
+    }
+
+    [HttpGet("{email}")]
+    public ActionResult<PassengerRm?> Find(string email)
+    {
+        var passenger = Passengers.FirstOrDefault(p => p.Email == email);
+        if (passenger == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(new PassengerRm(passenger.Email, passenger.FirstName, passenger.LastName, passenger.Gender));
+    }
+}
