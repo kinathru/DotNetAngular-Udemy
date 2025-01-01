82dfa63cfb36c6bde097335d2ef70321c6cde1bc
kinath <kinath.ru@gmail.com>
2025-01-01 13:14:44 -0600

    feat:implemented my booking page

--- flight-booking-app/src/app/my-bookings/my-bookings.component.ts
+++ flight-booking-app/src/app/my-bookings/my-bookings.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { BookingRm } from '../api/models';
+import { BookingService } from '../api/services';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-my-bookings',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-bookings.component.html',
-  styleUrl: './my-bookings.component.css'
+  styleUrl: './my-bookings.component.css',
 })
-export class MyBookingsComponent {
+export class MyBookingsComponent implements OnInit {
+  bookings!: BookingRm[];
 
+  constructor(
+    private bookingService: BookingService,
+    private authService: AuthService
+  ) {}
+  ngOnInit(): void {
+    this.bookingService
+      .listBooking({ email: this.authService.currentUser?.email ?? '' })
+      .subscribe({
+        next: (res) => {
+          this.bookings = res;
+        },
+        error: (err) => {
+          this.handleError(err);
+        },
+      });
+  }
+
+  private handleError(err: any) {
+    console.log('Response Error, Status: ', err.status);
+    console.log('Response Error, Status Text: ', err.statusText);
+    console.log(err);
+  }
+
+  cancel(booking: BookingRm) {}
 }
