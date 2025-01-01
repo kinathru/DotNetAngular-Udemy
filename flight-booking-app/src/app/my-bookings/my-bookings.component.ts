34cc4634b8f5849d7d8c48fc084452b786f5d036
kinath <kinath.ru@gmail.com>
2025-01-01 13:34:40 -0600

    feat:implemented cancel booking

--- flight-booking-app/src/app/my-bookings/my-bookings.component.ts
+++ flight-booking-app/src/app/my-bookings/my-bookings.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { BookingRm } from '../api/models';
+import { BookDto, BookingRm } from '../api/models';
 import { BookingService } from '../api/services';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
@@ -43,5 +43,20 @@ export class MyBookingsComponent implements OnInit {
     console.log(err);
   }
 
-  cancel(booking: BookingRm) {}
+  cancel(booking: BookingRm) {
+    const bookDto: BookDto = {
+      flightId: booking.flightId,
+      numberOfSeats: booking.numberOfBookedSeats,
+      passengerEmail: booking.passengerEmail,
+    };
+
+    this.bookingService.cancelBooking({ body: bookDto }).subscribe({
+      next: () => {
+        this.bookings = this.bookings.filter((b) => b != booking);
+      },
+      error: (err) => {
+        this.handleError(err);
+      },
+    });
+  }
 }
