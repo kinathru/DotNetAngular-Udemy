1650e37a131e3e269f90f4e0a2d0e633fd712460
kinath <kinath.ru@gmail.com>
2025-01-01 12:57:39 -0600

    feat:implemented booking controller

---
+++ Flight/WebAPI/Controllers/BookingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.ReadModels;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookingController(Entities entities) : ControllerBase
+{
+    [HttpGet("{email}")]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(IEnumerable<BookingRm>), StatusCodes.Status200OK)]
+    public ActionResult<IEnumerable<BookingRm>> List(string email)
+    {
+        var bookings = entities.Flights
+            .ToArray()
+            .SelectMany(f => f.Bookings.Where(b => b.PassengerEmail == email)
+                .Select(brm => new BookingRm(f.Id,
+                    f.Airline,
+                    f.Price,
+                    new TimePlaceRm(f.Arrival.Place, f.Arrival.Time),
+                    new TimePlaceRm(f.Departure.Place, f.Departure.Time),
+                    brm.NumberOfSeats,
+                    brm.PassengerEmail)));
+
+        return Ok(bookings);
+    }
+}
---
+++ Flight/WebAPI/ReadModels/BookingRm.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI.ReadModels;
+
+public record BookingRm(
+    Guid FlightId,
+    string Airline,
+    string Price,
+    TimePlaceRm Arrival,
+    TimePlaceRm Departure,
+    int NumberOfBookedSeats,
+    string PassengerEmail);
